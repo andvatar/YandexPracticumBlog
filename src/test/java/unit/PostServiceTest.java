597b1312ac9809c8be870c353837701bbfb4468e
Andrey Tarasov <andvatar@gmail.com>
2025-04-16 01:42:57 +0300

    tests with spring boot test

--- src/test/java/unit/PostServiceTest.java
+++ src/test/java/unit/PostServiceTest.java
@@ -1,14 +1,12 @@
 package unit;
 
-import configuration.UnitTestConfiguration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import ru.yandex.practicum.tarasov.model.Comment;
 import ru.yandex.practicum.tarasov.model.Post;
 import ru.yandex.practicum.tarasov.repository.CommentRepository;
@@ -22,13 +20,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-@ExtendWith({SpringExtension.class})
-@ContextConfiguration(classes = {UnitTestConfiguration.class})
-@ActiveProfiles("Test")
+@SpringBootTest(classes = PostService.class)
+@ActiveProfiles("test")
 class PostServiceTest {
-    @Autowired
+    @MockitoBean
     private PostRepository postRepository;
-    @Autowired
+    @MockitoBean
     private CommentRepository commentRepository;
     @Autowired
     private PostService postService;
