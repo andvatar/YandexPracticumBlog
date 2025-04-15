0239cbc449107bc9c324d7109b2ae3a66cc7aa5d
Andrey Tarasov <andvatar@gmail.com>
2025-04-16 00:23:01 +0300

    removed configurations

--- src/main/java/ru/yandex/practicum/tarasov/configuration/WebConfiguration.java
+++ src/main/java/ru/yandex/practicum/tarasov/configuration/WebConfiguration.java
@@ -12,18 +12,16 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = "ru.yandex.practicum.tarasov")
 @PropertySource("classpath:application.properties")
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Value("${spring.images.location}")
     private String imagesLocation;
 
-    @Bean
+    /*@Bean
     public MultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
-    }
+    }*/
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
